5e1e7872b332be15d10327b76d0cbc1f8a82a281
Elizabeth Blair <blair_pkfh@MonkadooCentral.(none)>
2026-09-15 21:59:09 -0600

    Realized my previous idea is stupid because I'm using standard 2d arrays that can't dynamically expand and copying the whole array over to add one new entry to offsets is more trouble than it's worth. As such, I have shifted back to just checking if the piece has moved yet if the space ahead of it is blank, then adding the new position only if the spot two spaces ahead is also empty.

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -14,10 +14,6 @@ public class PawnMovesCalculator implements PieceMovesCalculator{ //For pawns sp
                 offset[0] *= -1;
             }
         }
-        if((myPos.getRow() == 2 && myPiece.getTeamColor() == ChessGame.TeamColor.WHITE) || (myPos.getRow() == 7 && myPiece.getTeamColor() == ChessGame.TeamColor.BLACK)){
-            //The pawn hasn't moved yet, so we add an extra offset to check for double movement.
-            offsets[3] = new int[]{2, 0}; //Hopefully this adds things correctly.
-        }
 
         for(int offset[] : offsets){
             int offsetY = offset[0];
@@ -33,6 +29,10 @@ public class PawnMovesCalculator implements PieceMovesCalculator{ //For pawns sp
                         possibleMoves.add(new ChessMove(myPos, potentialPos, ChessPiece.PieceType.QUEEN));
                         possibleMoves.add(new ChessMove(myPos, potentialPos, ChessPiece.PieceType.KNIGHT));
                     } else{ //promo conditions not fulfilled
+                        if(atInitialPosition(myPos.getRow(), myPiece) && board.getPiece(new ChessPosition(spottedY + 1, spottedX)) == null){
+                            //it hasn't moved yet and the place 2 spaces ahead is also empty
+                            possibleMoves.add(new ChessMove(myPos, new ChessPosition(spottedY + 1, spottedX), null));
+                        }
                         possibleMoves.add(new ChessMove(myPos, potentialPos, null));
                     }
                 } else if(board.getPiece(myPos).getTeamColor() != board.getPiece(potentialPos).getTeamColor()){ //The occupied piece belongs to a different team
@@ -53,5 +53,9 @@ public class PawnMovesCalculator implements PieceMovesCalculator{ //For pawns sp
 
     private boolean promotionEligible(int row, ChessPiece myPiece){
         return (row == 8 && myPiece.getTeamColor() == ChessGame.TeamColor.WHITE) || (row == 1 && myPiece.getTeamColor() == ChessGame.TeamColor.BLACK);
+    }
+
+    private boolean atInitialPosition(int row, ChessPiece myPiece){
+        return (row == 2 && myPiece.getTeamColor() == ChessGame.TeamColor.WHITE) || (row == 7 && myPiece.getTeamColor() == ChessGame.TeamColor.BLACK);
     }
 }
